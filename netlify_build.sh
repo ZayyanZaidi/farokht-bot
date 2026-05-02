c7f8f075f0ee747a47f3debfdbe15f002607c04c
Zayyan Zaidi <zayyanzaidi57@gmail.com>
2026-05-03 01:16:57 +0500

    Add Netlify deployment configuration

---
+++ netlify_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. Install Flutter
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable
+fi
+
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# 2. Build the app
+cd app
+flutter pub get
+flutter build web --release
