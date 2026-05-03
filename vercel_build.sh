d2569256b003ad4ffc4624a91823633f418a7d9f
Zayyan Zaidi <zayyanzaidi57@gmail.com>
2026-05-03 12:50:29 +0500

    Fix: Workaround for pub.dev FormatException by bypassing separate pub get

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 export BOT=true
+export PUB_ENVIRONMENT=bot
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable --depth 1
 fi
@@ -9,7 +10,7 @@ flutter config --no-analytics
 flutter config --enable-web
 flutter --version
 cd app
-flutter pub get
+# flutter pub get # Skipping separate pub get to bypass advisories bug
 if [ -z "$BACKEND_URL" ]; then
   BACKEND_URL="https://farokht-bot-backend-784756226072.us-central1.run.app"
 fi
